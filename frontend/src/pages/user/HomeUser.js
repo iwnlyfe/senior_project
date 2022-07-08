fb088b50d0f6384cbf86a33319438faaef86440c
YokoTori123987 <nongtaop1@gmail.com>
2022-07-08 20:45:37 +0700

    create to productview and ui

--- frontend/src/pages/user/HomeUser.js
+++ frontend/src/pages/user/HomeUser.js
@@ -2,20 +2,20 @@ import React from 'react'
 export default function HomeUser() {
 
   return (
-    <div>
+    <div class='container-fluid'>
+      <div className='row'>
+        <div className=' col-2'>        
         <h1>Home User</h1>
-        {/* {JSON.stringify(products)} */}
-        <div className='col-2'>
           <ui>
-            <li className="list">
+            <li className="list-unstyled">
               <a href='productview'>
                 <span className='icon'>
 
                 </span>
-                <span className='list'>Peoduct</span>
+                <span className='list-item'>Peoduct</span>
               </a>
             </li>
-            <li className="list">
+            <li className="list-unstyled">
               <a href='#'>
                 <span>
 
@@ -23,7 +23,7 @@ export default function HomeUser() {
                 <span className='list'>productDetail</span>
               </a>
             </li>
-            <li className="list">
+            <li className="list-unstyled">
               <a href='#'>
                 <span>
 
@@ -31,23 +31,25 @@ export default function HomeUser() {
                 <span className='list'>disbursement</span>
               </a>
             </li>
-            <li className="list">
+            <li className="list-unstyled">
               <a href='#'>
                 <span>
-
                 </span>
-                <span className='list'>shelf</span>
+                <span className='list-item'>shelf</span>
               </a>
             </li>
-            <li className="list">
+            <li className="list-unstyled">
               <a href='#'>
                 <span>
-
                 </span>
                 <span className='list'>zone</span>
               </a>
             </li>
           </ui>
+        </div>
+        <div className='col-10 bg-info'>
+          <h1>Home</h1>
+        </div>      
         </div>
     </div>
   )
