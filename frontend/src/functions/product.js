84982ff637db8cbe58ce8f41dd26ffe1e86ccfd9
YokoTori123987 <nongtaop1@gmail.com>
2022-07-09 20:53:38 +0700

    edit to margin

--- frontend/src/functions/product.js
+++ frontend/src/functions/product.js
@@ -8,10 +8,15 @@ export const addProduct = async(value) =>{
     return await axios.post(process.env.REACT_APP_API + '/addProduct' , value)
 }
 
-export const updateProduct = async() =>{
-
-}
+// export const updateProduct = async(id,value) =>{
+    
+    // return await axios.
+// }
 
 export const deleteProduct = async(id) =>{
     return await axios.delete(process.env.REACT_APP_API + '/deleteProduct/' +id)
+}
+
+export const findOneProduct = async() =>{
+    return await axios.get(process.env.REACT_APP_API + '/findOneProduct/' + id)
 }
--- frontend/src/pages/mainehome/product/ProductView.js
+++ frontend/src/pages/mainehome/product/ProductView.js
@@ -3,6 +3,7 @@ import { useEffect,useState } from 'react'
 import {findAllProduct} from '../../../functions/product'
 import Menu from '../../../components/layout/Menu'
 import { deleteProduct } from '../../../functions/product'
+import { Link } from 'react-router-dom'
 
 export default function ProductView() {
   const [products,setProduct] = useState([])
@@ -67,8 +68,8 @@ export default function ProductView() {
       <Menu />
       <div  class='col-9 mx-2'>
         <div class='row'>
-          <h1 class='col-10'>Product View</h1>
-          <a href='productcreate' class='btn btn-outline-secondary py-0 my-3 btn-lg'>Create</a>
+          <h1 class='col-10 mt-3'>Product View</h1>
+          <a href='productcreate' class='btn btn-outline-secondary py-0 my-4 btn-lg'>Create</a>
         </div>
           
           <table class='table table-bordered table-light' >
@@ -85,12 +86,13 @@ export default function ProductView() {
             {products.map((product,index)=>(
             <tbody key={index}>
               <tr>
-                <th scope="row">1</th>
+                <th scope="row">{index +1}</th>
                 <th >{product.productName}</th>
                 <th >{product.quantity}</th>
                 <th >{product.price}</th>
                 <th >{product.group}</th>
                 <th>
+                  <Link></Link>
                   <a href='productupdate' className='btn btn-outline-warning btn-sm mx-1'>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
